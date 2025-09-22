b0dd84d6c4644425ed26d80b084fcd02dd487dcc
Thanh Tu <thanhtu10803@gmail.com>
2025-09-22 13:23:06 +0700

    Enhance pointer_address function with additional pointer demonstrations and comments

--- pointer.c
+++ pointer.c
@@ -2,13 +2,22 @@
 
 void pointer_address()
 {
-    int x = 1000; //0x03E8
+    int x = 1000; //0x03E8  -> save in memory: E8 03
     int* pt = &x;
     printf("&x: %d\n", &x);
     printf("pt: %d\n", pt);
     printf("&pt: %d\n", &pt);
     printf("*pt: %d\n", *pt);
+
+    unsigned char* ptt = &x;
+    printf("&x: %d\n", &x);
+    printf("ptt: %d\n", ptt);
+    printf("&ptt: %d\n", &ptt);
+    printf("*ptt: %d\n", *ptt);  //232 = 0xE8
+    printf("*ptt: %d\n", *(ptt+1));  //0x03 = 0x03
 }
+
+void 
 int main()
 {
     pointer_address();
