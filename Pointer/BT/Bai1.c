1be1a5066095385e31ba8acc14750a1bd9d9468e
Thanh Tu <thanhtu10803@gmail.com>
2025-09-22 21:32:57 +0700

    Feat(Bai1): Refactor enter_number and sum functions for clarity

--- Pointer/BT/Bai1.c
+++ Pointer/BT/Bai1.c
@@ -1,7 +1,24 @@
 #include <stdio.h>
-
+void enter_number(int* a, int*b)
+{
+    printf("Enter number a: ");
+    scanf("%d", a);
+    printf("Enter number b: ");
+    scanf("%d", b);
+}
+int* sum(int* a, int* b)
+{
+    static int sum;
+    sum = *a + *b;
+    return &sum;
+}
 
 int main()
 {
+    int a, b;
+    int* res;
+    enter_number(&a, &b);
+    res = sum(&a, &b);
+    printf("sum = %d", *res);
     return 0;
 }
