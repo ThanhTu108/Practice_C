9ea7a375b8909dd3c03cf0473e914d4ab38173b2
Thanh Tu <thanhtu10803@gmail.com>
2025-09-23 11:19:55 +0700

    Feat(Bai6): Implement string manipulation functions for input, target removal, and reversal

--- Pointer/BT/Bai6_string.c
+++ Pointer/BT/Bai6_string.c
@@ -1,7 +1,46 @@
 #include <stdio.h>
+#include <string.h>
+void str_get(char* pt, int size)
+{
+    printf("Enter string: ");
+    fgets(pt, size, stdin);
+    pt[strlen(pt) - 1] = '\0';
+}
+void str_rmv_target(char* pt, char* target)
+{
+    printf("Enter terget: ");
+    scanf("%c", target);
+    int temp = 0;
+    for(int i = 0; pt[i] != '\0'; i++)
+    {
+        if(*(pt+i) != *target)
+        {
+            pt[temp++] = pt[i];
+        }
+    }
+    pt[temp] = '\0';
+}
+void str_reverse(char* pt)
+{
+    int left = 0;
+    int right = strlen(pt) - 1;
+    while(left < right)
+    {
+        int temp = pt[left];
+        pt[left] = pt[right];
+        pt[right] = temp;
+        left++;
+        right--;
+    }
+}
 
 int main()
 {
-
+    char c[100];
+    char a;
+    str_get(c, 100);
+    str_rmv_target(c, &a);
+    str_reverse(c);
+    printf("%s", c);
     return 0;
 }
