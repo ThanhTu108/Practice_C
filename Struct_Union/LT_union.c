aadf14e353d213f6cd05e9c0b4ad1c24622c8426
Thanh Tu <thanhtu10803@gmail.com>
2025-09-23 15:49:13 +0700

    Feat(LT_union): Implement union type and demonstrate usage in main function

--- Struct_Union/LT_union.c
+++ Struct_Union/LT_union.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
+typedef union
+{
+    int i;
+    float f;
+    char s[10];
+} Type_t;
+
 int main()
 {
+    Type_t value;
+    value.i = 100;
+    value.f = 3.5;
+    strcpy(value.s, "hello");
+
+    printf("x = %d\n", value.i);
+    printf("f = %f\n", value.f);
+    printf("s = %s\n", value.s);
     return 0;
 }
