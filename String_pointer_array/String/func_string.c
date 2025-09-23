c7ddc49a2a00c52a6532055aae9ad4cd807deace
Thanh Tu <thanhtu10803@gmail.com>
2025-09-23 23:42:51 +0700

    Feat(String): implement function is alpha, is_upper

--- String_pointer_array/String/func_string.c
+++ String_pointer_array/String/func_string.c
@@ -9,6 +9,26 @@ int is_lower(char c)
     return 0;
 }
 
+int is_upper(char c)
+{
+    if(c>='A' && c<='Z')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+int is_alpha(char c)
+{
+    if((c>='a' && c<='z') || (c>='A' && c<='Z'))
+    {
+        return 1;
+    }
+    return 0;
+}
+
+
+
 int main()
 {
     char c = 'A';
