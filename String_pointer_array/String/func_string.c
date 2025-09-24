70c3c4d6084f53ac5a040b85cd0328eadfb12215
Thanh Tu <thanhtu10803@gmail.com>
2025-09-25 00:03:56 +0700

    feat(string): add is_digit, to_lower, and to_upper functions; improve main output format

--- String_pointer_array/String/func_string.c
+++ String_pointer_array/String/func_string.c
@@ -27,11 +27,37 @@ int is_alpha(char c)
     return 0;
 }
 
+int is_digit(char c)
+{
+    if(c >='0' && c<='9')
+    {
+        return 1;
+    }
+    return 0;
+}
+
+char to_lower(char c)
+{
+    if(c>='A' && c<='Z')
+    {
+        c+=32;
+    }
+    return c;
+}
+char to_upper(char c)
+{
+    if(c>='a' && c<='z')
+    {
+        c-=32;
+    }
+    return c;
+}
+
 
 
 int main()
 {
     char c = 'A';
-    printf("%d", is_lower(c));
+    printf("%d \n", is_lower(c));
     return 0;
 }
