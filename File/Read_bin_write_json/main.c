7df93cc7414bd4014be0a1bfeef280efefad4457
Thanh Tu <thanhtu10803@gmail.com>
2025-09-25 08:44:53 +0700

    feat(Read_bin_write_json): enhance file reading functions and add error handling

--- File/Read_bin_write_json/main.c
+++ File/Read_bin_write_json/main.c
@@ -43,15 +43,49 @@ int read_header(const char* path, s_header_t* header)
         return 0;
     }
     fread(header, sizeof(s_header_t), 1, f);
-    printf("Header: id=%d, version=%d, record_count=%d/n",
+    printf("Header: id=%d, version=%d, record_count=%d\n",
            header->id_file, header->version, header->number_record);
+    fclose(f);
     return 1;
 }
+
+int read_record(const char* path, s_header_t header, s_weatherRecord_t* record)
+{
+    FILE* f = fopen(path, "rb");
+
+    if(f == NULL)
+    {
+        printf("Can not open file\n");
+        fclose(f);
+        return 0;
+    }
+    fseek(f, sizeof(s_header_t), SEEK_SET);
+    int n = fread(record, sizeof(s_weatherRecord_t), header.number_record, f);
+    printf("Read %d records\n", n);
+    fclose(f);
+    return 1;
+}
+
 
 int main()
 {
     s_header_t header;
     char* path = "D:/Desktop/hoc/Tu/Training_lab/Practice_c/File/Read_bin_write_json/weather_data.bin";
-    read_header(path, &header);
+
+    if (!read_header(path, &header))
+        return 0;
+
+    s_weatherRecord_t* records = malloc(header.number_record * sizeof(s_weatherRecord_t));
+    if (!records) {
+        printf("Malloc failed\n");
+        return 0;
+    }
+
+    if (read_record(path, header, records)) {
+        printf("First record id=%u, battery=%u\n",
+               records[0].sensor_id, records[0].baterry);
+    }
+
+    free(records);
     return 0;
 }
