98e5fc26e82ebaac71a3b0ffed92f1163ec145c3
Thanh Tu <thanhtu10803@gmail.com>
2025-09-27 14:10:03 +0700

    fix(Bai2): correct memory allocation and access in getinfo function

--- Dynamic_memory_allocate/Bai2.c
+++ Dynamic_memory_allocate/Bai2.c
@@ -11,16 +11,16 @@ typedef struct
 info_t* getinfo(info_t* pt, int* num)
 {
     info_t* sv;
-    sv = malloc((*num +1) * sizeof(info_t));
+    sv = malloc((*num+1) * sizeof(info_t));
 
-    memcpy(sv, pt, sizeof(pt)* (*num));
-    sv[*num].name = malloc(sizeof(char));
-    sv[*num].sex = malloc(sizeof(char));
+    memcpy(sv, pt, sizeof(info_t)* (*num));
+    sv[*num].name = malloc(100);
+    sv[*num].sex = malloc(100);
     
     printf("Name: ");
     gets(sv[*num].name);
     printf("Age: ");
-    scanf("%d", &sv->age);
+    scanf("%d", &sv[*num].age);
     fflush(stdin);
     printf("Sex: ");
     gets(sv[*num].sex);
@@ -30,7 +30,31 @@ info_t* getinfo(info_t* pt, int* num)
     return sv;
 }
 
+info_t* deleteInfo(info_t* pt, int* num)
+{
+    char* target = malloc(100);
+    printf("Enter target name: ");
+    gets(target);
 
+    for(int  i = 0; i < *num; i++)
+    {
+        if(strcmp(pt[i].name, target) == 0)
+        {
+            free(pt[i].name);
+            free(pt[i].sex);
+            for(int j = i; j<*num - 1; j++)
+            {
+                pt[j] = pt[j+1];
+            }
+            (*num)--;
+            i--;
+        }
+    }
+
+    info_t* sv = realloc(pt, (*num)*sizeof(info_t));
+    free(target);    
+    return sv;
+}
 
 void printinfo(info_t* pt, int num)
 {
@@ -42,11 +66,14 @@ void printinfo(info_t* pt, int num)
     printf("\n");
 }
 
+
 int main()
 {
     info_t* sv1 = NULL;
     int n = 0;
     sv1 = getinfo(sv1, &n);
+    sv1 = getinfo(sv1, &n);
+    sv1 = deleteInfo(sv1, &n);
     printinfo(sv1, n);
     free(sv1);
     return 0;
