3bd34039400486eab0bc5071a90d2152338e0bdb
Thanh Tu <thanhtu10803@gmail.com>
2025-09-28 11:24:58 +0700

    fix: correct memory management in 2D array functions and improve input handling

--- Pointer_to_pointer/BT/bai1.c
+++ Pointer_to_pointer/BT/bai1.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <stdlib.h>
 void arr_2d_get(int** pt, int row, int col)
 {
     for(int i = 0; i<row; i++)
@@ -11,10 +11,49 @@ void arr_2d_get(int** pt, int row, int col)
         }
     }
 }
-
-void arr_2d_remove(int** pt, int row, int col)
+void arr_2d_remove(int** pt, int* row, int col, int idx)
 {
-    
+    free(pt[idx]);
+    for(int i = idx; i < *row - 1; i++)
+    {
+        pt[i] = pt[i+1];
+    }   
+    (*row)--;
+}
+
+int** arr_2d_add(int** pt, int* row, int col, int idx)
+{
+    int** new_arr;
+    new_arr = (int**)malloc((*row + 1) * sizeof(int*));
+    for(int i = 0; i < *row+1; i++)
+    {
+        new_arr[i] = (int*)malloc(col * sizeof(int));
+    } 
+    for(int i = 0; i < idx; i++)
+    {
+        for(int j = 0; j<col; j++)
+        {
+            new_arr[i][j] = pt[i][j];
+        }
+    }
+    for(int i=idx; i<*row; i++)
+    {   
+        for (int j = 0; j < col; j++) {
+            new_arr[i+1][j] = pt[i][j];
+        }
+    }
+    for(int i = 0 ;i < col; i++)
+    {
+        printf("Arr[%d][%d]: ", idx, i);
+        scanf("%d", &new_arr[idx][i]);
+    }
+    (*row) ++ ;
+
+    for (int i = 0; i < *row; i++) {
+        free(pt[i]);
+    }
+    free(pt);
+    return new_arr;
 }
 
 void arr_2d_print(int** pt, int row, int col)
@@ -25,13 +64,13 @@ void arr_2d_print(int** pt, int row, int col)
         {
             printf("%-4d", pt[i][j]);
         }
-        print("\n");
+        printf("\n");
     }
 }
 
 int main()
 {
-    int row = 5, col = 5;
+    int row = 3, col = 3;
     int** pt = (int**)malloc(row * sizeof(int*));
     for(int i = 0; i<row; i++)
     {
@@ -39,6 +78,17 @@ int main()
     }
 
     arr_2d_get(pt, row, col);
+    arr_2d_remove(pt, &row, col, 1);
+    pt = arr_2d_add(pt, &row, col, 1);
     arr_2d_print(pt, row, col);
+
+
+
+    for(int i = 0; i<row; i++)
+    {
+        free(pt[i]);
+    }
+
+    free(pt);
     return 0;
 }
