ac8c5c94c48c42ff382fd8d9cdd944e49195e6d4
Thanh Tu <thanhtu10803@gmail.com>
2025-09-30 09:52:45 +0700

    fix(Call back): add null check for UART data callback to prevent dereferencing null pointer

--- Pointer_to_pointer/controham/Callback/lib.c
+++ Pointer_to_pointer/controham/Callback/lib.c
@@ -5,7 +5,10 @@ data_uart_handeler_t data_ptr = NULL;
 
 void Uart_RX_data(char* data_Rx, int data_len)
 {
-    data_ptr(data_Rx, data_len);
+    if(data_Rx)
+    {
+        data_ptr(data_Rx, data_len);
+    }
 }
 
 void Uart_Rx_data_set_callback(data_uart_handeler_t fptr)
