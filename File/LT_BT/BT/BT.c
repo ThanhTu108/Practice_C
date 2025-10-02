a23b8946cd6f68b8dd1e90a968fad272a294a963
Thanh Tu <thanhtu10803@gmail.com>
2025-10-02 13:38:34 +0700

    feat: Enhance student management system with file read/write functionality and improve data handling

--- File/LT_BT/BT/BT.c
+++ File/LT_BT/BT/BT.c
@@ -5,6 +5,7 @@
 
 int main()
 {
+    const char* path = "D:\\Desktop\\hoc\\Tu\\Training_lab\\Practice_c\\File\\LT_BT\\BT\\Sv.txt";
     int num = 0;
     struct Func f = 
     {
@@ -12,17 +13,78 @@ int main()
         .print = std_print,
         .sort_avr = std_sort_avr,
         .sort_name = std_sort_name,
+        .read_file = read_file,
+        .write_file = write_file,
     };
     struct Info_t* sv = NULL;
-    sv = calloc(3 , sizeof(struct Info_t*));
-    sv = f.get(sv, &num);
-    sv = f.get(sv, &num);
-    printf("Danh sách ban đầu:\n");
-    f.print(sv, num);
+    sv = f.read_file(path, sv, &num);
+    if (sv == NULL || num == 0) 
+    {
+        printf("File empty\n");
+    } 
+    else 
+    {
+        f.print(sv, num);
+    }
+    int start_idx = num;
+    int choose;
+    while (1) 
+    {
+        printf("\n========== MENU ==========\n");
+        printf("1. Get students\n");
+        printf("2. Print students\n");
+        printf("3. Sort name\n");
+        printf("4. Sort array\n");
+        printf("5. Save file\n");
+        printf("6. Read file\n");
+        printf("0. Exit\n");
+        printf("Choose: ");
+        scanf("%d", &choose);
 
-    printf("Sap xep: \n");
-    f.sort_name(sv, num);
-    f.print(sv, num);
+        switch (choose) 
+        {
+            case 1:
+                sv = f.get(sv, &num);
+                break;
+            case 2:
+                f.print(sv, num);
+                break;
+            case 3:
+                f.sort_name(sv, num);
+                f.print(sv, num);
+                printf("Name Sort done\n");
+                break;
+            case 4:
+                f.sort_avr(sv, num);
+                f.print(sv, num);
+                printf("Avr Sort done\n");
+                break;
+            case 5:
+                f.write_file(path, sv, start_idx, num);
+                printf("Save file done!!!\n");
+                start_idx = num;
+                break;
+            case 6:
+                if (sv == NULL || num == 0) 
+                {
+                    printf("File empty\n");
+                } 
+                else 
+                {
+                    f.print(sv, num);
+                }
+                break;
+            case 0:
+                printf("Exit\n");
+                free(sv);
+                return 0;
+            default:
+                printf("Error\n");
+        }
+    }
+
+   
+    
     free(sv);
     return 0;
 }
--- File/LT_BT/BT/lib.h
+++ File/LT_BT/BT/lib.h
@@ -17,7 +17,9 @@ struct Info_t
 
 typedef struct Info_t* (*Get_func)(struct Info_t*, int*);
 typedef void(*Active_Func)(struct Info_t*, int);
-typedef struct Info_t* (*File_func)(const char*,struct Info_t*, int);
+
+typedef struct Info_t* (*File_func)(const char*,struct Info_t*, int*);
+typedef void(*Write_func)(const char*, struct Info_t*, int, int);
 struct Func
 {
     Get_func get;
@@ -25,11 +27,13 @@ struct Func
     Active_Func sort_avr;
     Active_Func sort_name;
     File_func read_file;
+    Write_func write_file;
 };
 
 struct Info_t* std_get(struct Info_t* sv, int* num);
 void std_print(struct Info_t* sv, int num);
 void std_sort_avr(struct Info_t* sv, int num);
 void std_sort_name(struct Info_t* sv, int num);
-struct Info_t* read_file(const char* path, struct Info_t* sv, int num);
+struct Info_t* read_file(const char* path, struct Info_t* sv, int* num);
+void write_file(const char* path, struct Info_t* sv, int start_idx ,int num);
 #endif 
